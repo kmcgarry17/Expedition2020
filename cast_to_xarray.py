b8eb10a8141f9b979020ad9ca40e7e4ce30f28ec
kmcgarry17 <43968007+kmcgarry17@users.noreply.github.com>
2020-11-13 10:26:30 -0500

    add function
    upload the function as a pure python script and take it out of the notebook

---
+++ cast_to_xarray.py
@@ -0,0 +1,32 @@
+import os
+import pandas as pd
+import xarray as xr
+import numpy as np
+import matplotlib.pyplot as plt
+import cmocean
+import gsw
+from seabird.cnv import fCNV
+
+def cast_to_xarray(file, stnno):
+    cast = fCNV(file)
+    
+    # get data from cnv file
+    depth    = cast['DEPTH']
+    temperature = cast['TEMP']
+    salinity    = cast['PSAL']
+    
+    # put the data in a dictionary
+    datadic = { 'depth' : depth, 'temperature' : temperature,
+               'salinity' : salinity}
+    
+    # convert the dictionary to a pandas dataframe
+    castdf=pd.DataFrame.from_dict(datadic)
+    
+    # convert pandas to xarray
+    castxr = castdf.set_index('depth').to_xarray()
+    
+    castxr = castxr.assign_coords({'latitude': xr.DataArray(cast.attributes['LATITUDE']),
+                                   'longitude' : xr.DataArray(cast.attributes['LONGITUDE']),
+                                 'station':stnno})
+    
+    return castxr
